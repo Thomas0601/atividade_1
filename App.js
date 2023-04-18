e847af95339768018898e696af737e32dd58cb05
Thomas Lineker <129558810+Thomas0601@users.noreply.github.com>
2023-04-17 23:59:56 -0300

    questâo_mobile

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (value === 'Ligado') {
+      setShowMessage(true);
+    } else {
+      setShowMessage(false);
+    }
+  }, [value]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input}
+        placeholder="Digite a palavra: Ligado"
+        onChangeText={setValue}
+        value={value}
+      />
+      {showMessage && (
+        <Text style={styles.text}>Apresentando o texto.</Text>
+      )}
     </View>
   );
 }
@@ -13,8 +30,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00cbff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 46,
+    width: 350,
+    borderWidth: 3,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    borderColor: 'black',
+    backgroundColor: 'white',
+
+
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
